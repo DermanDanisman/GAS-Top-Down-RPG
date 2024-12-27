891635e114ddf77808c53747854dc328b0afda7c
Derman Can Danisman <dermancanwow@gmail.com>
2024-12-27 19:00:54 +0300

    Player Character and Enemy Character Blueprint Setup

--- Source/GAS_TopDownRPG/Private/Character/GASCharacterBase.cpp
+++ Source/GAS_TopDownRPG/Private/Character/GASCharacterBase.cpp
@@ -3,12 +3,17 @@
 
 #include "Character/GASCharacterBase.h"
 
+#include "Preferences/PersonaOptions.h"
+
 // Sets default values
 AGASCharacterBase::AGASCharacterBase()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
-
+	
+	WeaponMesh = CreateDefaultSubobject<USkeletalMeshComponent>("WeaponMesh");
+	WeaponMesh->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	WeaponMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 // Called when the game starts or when spawned
