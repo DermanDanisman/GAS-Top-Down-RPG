eaadf84e9ac525997504b366faaf3bdc4300b1c7
Derman Can Danisman <dermancanwow@gmail.com>
2025-01-03 18:48:35 +0300

    Setting Attributes and Attribute Accessors
    Setting Attributes and Attribute Accessors in Attribute set class.

--- Source/GAS_TopDownRPG/Private/AbilitySystem/GASAttributeSet.cpp
+++ Source/GAS_TopDownRPG/Private/AbilitySystem/GASAttributeSet.cpp
@@ -3,3 +3,40 @@
 
 #include "AbilitySystem/GASAttributeSet.h"
 
+#include "AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
+
+UGASAttributeSet::UGASAttributeSet()
+{
+	
+}
+
+void UGASAttributeSet::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UGASAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UGASAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UGASAttributeSet, Mana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UGASAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+}
+
+void UGASAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UGASAttributeSet, Health, OldHealth);
+}
+
+void UGASAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UGASAttributeSet, MaxHealth, OldMaxHealth);
+}
+
+void UGASAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UGASAttributeSet, Mana, OldMana);
+}
+
+void UGASAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UGASAttributeSet, MaxMana, OldMaxMana);
+}
--- Source/GAS_TopDownRPG/Public/AbilitySystem/GASAttributeSet.h
+++ Source/GAS_TopDownRPG/Public/AbilitySystem/GASAttributeSet.h
@@ -7,11 +7,54 @@
 #include "GASAttributeSet.generated.h"
 
 /**
- * 
+  * Attributes:
+	In Gameplay Ability System (GAS), Attributes and Attribute Sets are essential components that manage and represent various character stats,
+	such as health, mana, stamina, damage, and more. These attributes can be modified through gameplay effects and are closely tied to abilities and the overall game mechanics.
+	Attributes have the type FGameplayAttributeData.
+
+	FGameplayAttributeData: This is a struct that holds a single value for the attribute (e.g., a float for health).
+	ATTRIBUTE_ACCESSORS: This is a macro that generates getter and setter functions for each attribute, making it easier to access and modify them.
+
+	Summary:
+	Attributes are the core stats (e.g., health, mana, stamina) that directly impact gameplay.
+	Attribute Set is a container for the attributes and provides functions to access and modify them.
+	Gameplay Abilities interact with these attributes, modifying them through Gameplay Effects.
+	Replication ensures that attribute values are consistent across clients in multiplayer games.
  */
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
 UCLASS()
 class GAS_TOPDOWNRPG_API UGASAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+
+public:
+	UGASAttributeSet();
+
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+
+
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth);
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldMana);
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana);
 	
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_HealthChanged, Category = "Vital Attributes")
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(UGASAttributeSet, Health)
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealthChanged, Category = "Vital Attributes")
+	FGameplayAttributeData MaxHealth;
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_ManaChanged, Category = "Vital Attributes")
+	FGameplayAttributeData Mana;
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxManaChanged, Category = "Vital Attributes")
+	FGameplayAttributeData MaxMana;
 };
