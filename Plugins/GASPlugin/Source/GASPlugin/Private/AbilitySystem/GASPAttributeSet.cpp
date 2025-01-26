3e8fcc13093fb9b0187ef748a32c976e85b030a9
Derman Can Danisman <dermancanwow@gmail.com>
2025-01-26 22:24:47 +0300

    Implemented PreAttributeChange, PreAttributeBaseChange and PostGameplayEffectExecute to Attribute Set Class
    Clamped values using PreAttributeChange and PreAttributeBaseChange to prevent exceeding limits (e.g., max health, max mana).
    
    Implemented PostGameplayEffectExecute to store critical data about gameplay effects.
    
    Added a struct FGameplayEffectProperties to hold both source and target actor information for gameplay effects.
    
    This update enhances attribute clamping functionality to ensure that the health and mana attributes do not exceed their maximum values during gameplay. This is achieved through the PreAttributeChange and PreAttributeBaseChange functions, which modify values before they're committed to attributes. Additionally, the PostGameplayEffectExecute function was implemented to capture and store important information about gameplay effects (source, target, and related attributes). A new struct, FGameplayEffectProperties, was created to bundle together data related to both the source and target of the gameplay effects, improving the clarity and management of effect-related data.

--- Plugins/GASPlugin/Source/GASPlugin/Private/AbilitySystem/GASPAttributeSet.cpp
+++ Plugins/GASPlugin/Source/GASPlugin/Private/AbilitySystem/GASPAttributeSet.cpp
@@ -3,6 +3,9 @@
 
 #include "AbilitySystem/GASPAttributeSet.h"
 
+#include "AbilitySystemBlueprintLibrary.h"
+#include "GameplayEffectExtension.h"
+#include "GameFramework/Character.h"
 #include "Net/UnrealNetwork.h"
 
 UGASPAttributeSet::UGASPAttributeSet()
@@ -34,6 +37,125 @@ void UGASPAttributeSet::GetLifetimeReplicatedProps(TArray<class FLifetimePropert
 	DOREPLIFETIME_CONDITION_NOTIFY(UGASPAttributeSet, Stamina, COND_None, REPNOTIFY_Always);
 	DOREPLIFETIME_CONDITION_NOTIFY(UGASPAttributeSet, MaxStamina, COND_None, REPNOTIFY_Always);
 	DOREPLIFETIME_CONDITION_NOTIFY(UGASPAttributeSet, StaminaRegenRate, COND_None, REPNOTIFY_Always);
+}
+
+void UGASPAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+	Super::PreAttributeChange(Attribute, NewValue);
+
+	if (Attribute == GetHealthAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0.f, GetMaxHealth());
+		UE_LOG(LogTemp, Warning, TEXT("Health: %f"), NewValue);
+	}
+
+	if (Attribute == GetMaxHealthAttribute())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Max Health: %f"), NewValue);
+	}
+
+	if (Attribute == GetHealthRegenRateAttribute())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Health Regen: %f"), NewValue);
+	}
+
+	if (Attribute == GetManaAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0.f, GetMaxMana());
+		UE_LOG(LogTemp, Warning, TEXT("Mana: %f"), NewValue);
+	}
+
+	if (Attribute == GetMaxManaAttribute())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Max Mana: %f"), NewValue);
+	}
+
+	if (Attribute == GetManaRegenRateAttribute())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Mana Regen: %f"), NewValue);
+	}
+}
+
+void UGASPAttributeSet::PreAttributeBaseChange(const FGameplayAttribute& Attribute, float& NewValue) const
+{
+	Super::PreAttributeBaseChange(Attribute, NewValue);
+
+	if (Attribute == GetHealthAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0.f, GetMaxHealth());
+		UE_LOG(LogTemp, Warning, TEXT("Base Health: %f"), NewValue);
+	}
+
+	if (Attribute == GetMaxHealthAttribute())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Base Max Health: %f"), NewValue);
+	}
+
+	if (Attribute == GetHealthRegenRateAttribute())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Base Health Regen: %f"), NewValue);
+	}
+
+	if (Attribute == GetManaAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0.f, GetMaxMana());
+		UE_LOG(LogTemp, Warning, TEXT("Base Mana: %f"), NewValue);
+	}
+
+	if (Attribute == GetMaxManaAttribute())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Base Max Mana: %f"), NewValue);
+	}
+
+	if (Attribute == GetManaRegenRateAttribute())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Base Mana Regen: %f"), NewValue);
+	}
+}
+
+void UGASPAttributeSet::SetGameplayEffectProperties(const struct FGameplayEffectModCallbackData& Data,
+	FGameplayEffectProperties& EffectProperties) const
+{
+	// Source = causer of the effect, Target = target of the effect (owner of this Attribute Set)
+	
+	EffectProperties.GameplayEffectContextHandle = Data.EffectSpec.GetContext();
+	EffectProperties.SourceAbilitySystemComponent = EffectProperties.GameplayEffectContextHandle.GetOriginalInstigatorAbilitySystemComponent();
+
+	// Capture source actor info
+	if (IsValid(EffectProperties.SourceAbilitySystemComponent) && EffectProperties.SourceAbilitySystemComponent->AbilityActorInfo.IsValid()
+		&& EffectProperties.SourceAbilitySystemComponent->AbilityActorInfo->AvatarActor.IsValid())
+	{
+		EffectProperties.SourceAvatarActor = EffectProperties.SourceAbilitySystemComponent->AbilityActorInfo->AvatarActor.Get();
+		EffectProperties.SourceController = EffectProperties.SourceAbilitySystemComponent->AbilityActorInfo->PlayerController.Get();
+		if (EffectProperties.SourceController == nullptr && EffectProperties.SourceAvatarActor != nullptr)
+		{
+			if (const APawn* SourcePawn = Cast<APawn>(EffectProperties.SourceAvatarActor))
+			{
+				EffectProperties.SourceController = SourcePawn->GetController();
+			}
+		}
+		if (EffectProperties.SourceController)
+		{
+			ACharacter* SourceCharacter = Cast<ACharacter>(EffectProperties.SourceController->GetPawn());
+		}
+	}
+
+	// Capture target actor info
+	if (Data.Target.AbilityActorInfo.IsValid() && Data.Target.AbilityActorInfo->AvatarActor.IsValid())
+	{
+		EffectProperties.TargetAvatarActor = Data.Target.AbilityActorInfo->AvatarActor.Get();
+		EffectProperties.TargetController = Data.Target.AbilityActorInfo->PlayerController.Get();
+		EffectProperties.TargetCharacter = Cast<ACharacter>(EffectProperties.TargetAvatarActor);
+		UAbilitySystemComponent* TargetAbilitySystemComponent = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(EffectProperties.TargetAvatarActor);
+	}
+}
+
+void UGASPAttributeSet::PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData& Data)
+{
+	Super::PostGameplayEffectExecute(Data);
+
+	FGameplayEffectProperties EffectProperties;
+	SetGameplayEffectProperties(Data, EffectProperties);
 }
 
 void UGASPAttributeSet::OnRep_HealthChanged(const FGameplayAttributeData& OldHealth) const
--- Plugins/GASPlugin/Source/GASPlugin/Public/AbilitySystem/GASPAttributeSet.h
+++ Plugins/GASPlugin/Source/GASPlugin/Public/AbilitySystem/GASPAttributeSet.h
@@ -13,6 +13,23 @@ In Gameplay Ability System (GAS), Attributes and Attribute Sets are essential co
 such as health, mana, stamina, damage, and more. These attributes can be modified through gameplay effects and are closely tied to abilities and the overall game mechanics.
 Attributes have the type FGameplayAttributeData.
 
+Attributes have 2 protected member variables called BaseValue and Current Value. The BaseValue is the permanent value of an attribute,
+while the CurrentValue is the BaseValue plus any temporary modifications caused by GameplayEffects.
+
+Base Value:
+
+	This is the permanent value of an attribute.
+	It represents the starting, unmodified value.
+	For example, the Base Health is the original health value, without any gameplay effects that temporarily modify it (like buffs or debuffs).
+	The Base Value can be modified by permanent effects, such as leveling up, equipment, or permanent buffs.
+
+Current Value:
+
+	This is the value that can fluctuate during gameplay, depending on temporary effects.
+	For instance, if a gameplay effect (like a health potion or buff) is applied, it will modify the Current Health without changing the Base Health.
+	Current Value is dynamic and can be modified by temporary buffs, debuffs, or in-game actions.
+	The Current Value will always be subject to clamping if you want to prevent it from exceeding certain limits (like Max Health).
+
 FGameplayAttributeData: This is a struct that holds a single value for the attribute (e.g., a float for health).
 ATTRIBUTE_ACCESSORS: This is a macro that generates getter and setter functions for each attribute, making it easier to access and modify them.
 
@@ -24,8 +41,8 @@ Replication ensures that attribute values are consistent across clients in multi
 */
 
 /**
-* This defines a set of helper functions for accessing and initializing attributes, to avoid having to manually write these functions.
-* It would create the following functions, for attribute Health
+* Macro to define getter, setter, and initialization functions for attributes in the attribute set.
+* It automatically creates functions to get, set, and initialize attributes, reducing boilerplate code.
 *
 *	static FGameplayAttribute UMyHealthSet::GetHealthAttribute();
 *	FORCEINLINE float UMyHealthSet::GetHealth() const;
@@ -39,6 +56,65 @@ GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
 GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
 GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
+// Struct to hold actor, controller, and character data for both source and target of gameplay effects.
+USTRUCT(BlueprintType)
+struct FGameplayEffectProperties
+{
+	GENERATED_BODY()
+	
+	/** 
+	 * The context handle that contains the effect's context data. It stores information about the effect, such as its origin.
+	 */
+	UPROPERTY(BlueprintReadOnly, Category = "GASP Plugin | Ability System | Gameplay Effect Properties")
+	FGameplayEffectContextHandle GameplayEffectContextHandle;
+
+	// Source Actor Info
+	/** The ability system component of the source actor, used to apply and modify attributes and effects. */
+	UPROPERTY(BlueprintReadOnly, Category = "GASP Plugin | Ability System | Gameplay Effect Properties | Source")
+	TObjectPtr<UAbilitySystemComponent> SourceAbilitySystemComponent;
+
+	/** The actor who applied the gameplay effect (e.g., the character or pawn that triggered the effect). */
+	UPROPERTY(BlueprintReadOnly, Category = "GASP Plugin | Ability System | Gameplay Effect Properties | Source")
+	TObjectPtr<AActor> SourceAvatarActor;
+
+	/** The controller of the source actor, which could be a player or AI controller. */
+	UPROPERTY(BlueprintReadOnly, Category = "GASP Plugin | Ability System | Gameplay Effect Properties | Source")
+	TObjectPtr<AController> SourceController;
+
+	/** The character associated with the source actor. Used to link gameplay effects to character-specific behavior. */
+	UPROPERTY(BlueprintReadOnly, Category = "GASP Plugin | Ability System | Gameplay Effect Properties | Source")
+	TObjectPtr<ACharacter> SourceCharacter;
+
+	// Target Actor Info
+	/** The ability system component of the target actor, used to modify target attributes and apply effects. */
+	UPROPERTY(BlueprintReadOnly, Category = "GASP Plugin | Ability System | Gameplay Effect Properties | Target")
+	TObjectPtr<UAbilitySystemComponent> TargetAbilitySystemComponent;
+
+	/** The actor receiving the gameplay effect. This could be an enemy, NPC, or another player. */
+	UPROPERTY(BlueprintReadOnly, Category = "GASP Plugin | Ability System | Gameplay Effect Properties | Target")
+	TObjectPtr<AActor> TargetAvatarActor;
+
+	/** The controller associated with the target actor. Used for managing the target's actions. */
+	UPROPERTY(BlueprintReadOnly, Category = "GASP Plugin | Ability System | Gameplay Effect Properties | Target")
+	TObjectPtr<AController> TargetController;
+
+	/** The character associated with the target actor, useful for applying gameplay effects to character behavior. */
+	UPROPERTY(BlueprintReadOnly, Category = "GASP Plugin | Ability System | Gameplay Effect Properties | Target")
+	TObjectPtr<ACharacter> TargetCharacter;
+
+	// Constructor to initialize all pointers to null.
+	FGameplayEffectProperties()
+		: SourceAbilitySystemComponent(nullptr)
+		, SourceAvatarActor(nullptr)
+		, SourceController(nullptr)
+		, SourceCharacter(nullptr)
+		, TargetAbilitySystemComponent(nullptr)
+		, TargetAvatarActor(nullptr)
+		, TargetController(nullptr)
+		, TargetCharacter(nullptr)
+	{}
+};
+
 UCLASS()
 class GASPLUGIN_API UGASPAttributeSet : public UAttributeSet
 {
@@ -49,6 +125,12 @@ public:
 	UGASPAttributeSet();
 
 	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+
+	virtual void PreAttributeBaseChange(const FGameplayAttribute& Attribute, float& NewValue) const override;
+
+	virtual void PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData& Data) override;
 
 
 	/** The OnRep_* functions are callbacks that are executed when the corresponding attribute is updated.
@@ -73,7 +155,11 @@ public:
 	void OnRep_MaxStaminaChanged(const FGameplayAttributeData& OldMaxStamina) const;
 	UFUNCTION()
 	void OnRep_StaminaRegenRateChanged(const FGameplayAttributeData& OldStaminaRegenRate) const;
-	
+
+	/** 
+	 * These functions are executed when the attribute is replicated.
+	 * They are useful for triggering visual effects, like updating UI elements when an attribute value changes.
+	 */
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_HealthChanged, Category = "GASP Plugin | Vital Attributes", Meta=(DisplayName="VitalAttributes.Health"))
 	FGameplayAttributeData Health;
 	ATTRIBUTE_ACCESSORS(UGASPAttributeSet, Health);
@@ -110,4 +196,9 @@ public:
 	FGameplayAttributeData StaminaRegenRate;
 	ATTRIBUTE_ACCESSORS(UGASPAttributeSet, StaminaRegenRate);
 
+protected:
+
+	/** Function to fill in the effect properties after a gameplay effect executes */
+	virtual void SetGameplayEffectProperties(const struct FGameplayEffectModCallbackData& Data, FGameplayEffectProperties& EffectProperties) const;
 };
+
